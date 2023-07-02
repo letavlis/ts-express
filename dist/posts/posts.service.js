990881cb0cd5bce8e80249d177d82bbf60e67027
Bruna Leticia <bruna.letavlis@outlook.com>
2023-07-02 15:47:03 -0300

    Feat: Memory data

---
+++ dist/posts/posts.service.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let posts = {
+    1: {
+        id: 1,
+        nome: "Post 1",
+        descricao: "Descrição do Post 1",
+        categoria: "Categoria 1"
+    },
+    2: {
+        id: 2,
+        nome: "Post 2",
+        descricao: "Descrição do Post 2",
+        categoria: "Categoria 2"
+    }
+};
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,19 @@
+import { BasePost, Post } from "./post.interface";
+import { Posts } from "./posts.interface";
+
+let posts: Posts = {
+    1:{
+        id: 1,
+        nome: "Post 1",
+        descricao: "Descrição do Post 1",
+        categoria: "Categoria 1"
+    },
+    2:{
+        id: 2,
+        nome: "Post 2",
+        descricao: "Descrição do Post 2",
+        categoria: "Categoria 2"
+
+    }
+}
+
